bbe56e8a7fcbfe4aac69c7dfc4b54a77fcd2bdc5
Mario Loyola <mar.loyolac@duocuc.cl>
2026-03-25 00:01:29 -0300

    Initial commit

---
+++ backend/src/main/java/fullstack/caso_semestral/CasoSemestralApplication.java
@@ -0,0 +1,13 @@
+package fullstack.caso_semestral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasoSemestralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasoSemestralApplication.class, args);
+	}
+
+}
